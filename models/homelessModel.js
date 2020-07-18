cb67ddfe0342a064d454560caba2f8881577ddac
Mattar <acemattar@gmail.com>
2020-07-18 06:25:15 +0200

    feat: basic homeless model

---
+++ models/homelessModel.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const HomelessSchema = new mongoose.Schema({
+	author: {
+		type: String,
+		required: [ true, 'A case published must have an author' ]
+	},
+	description: {
+		type: String,
+		required: [ true, 'A case published must have a brief description' ]
+	},
+	medicalCare: {
+		type: Boolean,
+		required: [ true, 'A case published must have a medical status' ]
+	},
+	comments: [ { body: String, date: Date } ],
+	date: { type: Date, default: Date.now }
+});
+
+const Homeless = mongoose.model('Homeless', HomelessSchema);
+
+// const testHomeless = new Homeless({
+// 	author: 'ahmed mattar',
+// 	description: 'old guy in a very bad shape',
+// 	medicalCare: true
+// });
+
+// testHomeless
+// 	.save()
+// 	.then((doc) => {
+// 		console.log(doc);
+// 	})
+// 	.catch((err) => {
+// 		console.log('Error:', err);
+// 	});
--- server.js
+++ server.js
@@ -1,25 +1,22 @@
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-dotenv.config({ path: "./config.env" });
-const app = require("./app");
+dotenv.config({ path: './config.env' });
+const app = require('./app');
 
-const DB = process.env.DATABASE.replace(
-  "<PASSWORD>",
-  process.env.DATABASE_PASSWORD
-);
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
 mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("DB connection successful!"));
+	.connect(DB, {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useFindAndModify: false,
+		useUnifiedTopology: true
+	})
+	.then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(` server is up and running, listening on port ${port}`);
+	console.log(` server is up and running, listening on port ${port}`);
 });
