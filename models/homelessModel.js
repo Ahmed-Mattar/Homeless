de0a4c7f3110ff03f1a2f690ae90f0f815de002b
Mattar <acemattar@gmail.com>
2020-07-19 16:26:45 +0200

    feat: using validators on homeless model

--- models/homelessModel.js
+++ models/homelessModel.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const HomelessSchema = new mongoose.Schema({
 	name: {
-		type: String
+		type: String,
+		trim: true,
+		maxlength: [ 40, 'A name must have less or equal than 40 characters' ],
+		minlength: [ 10, 'A name must have more or equal than 10 characters' ],
+		validate: [ validator.isAlpha, 'name must only contain characters ' ]
 	},
 	author: {
 		type: String,
@@ -11,7 +16,8 @@ const HomelessSchema = new mongoose.Schema({
 	description: {
 		type: String,
 		required: [ true, 'A case published must have a brief description' ],
-		trim: true
+		trim: true,
+		maxlength: [ 150, 'A description of a case must have more or equal to 150 character' ]
 	},
 	medicalCare: {
 		type: Boolean,
@@ -19,7 +25,10 @@ const HomelessSchema = new mongoose.Schema({
 	},
 	gender: {
 		type: String,
-		enum: [ 'Male', 'Female' ],
+		enum: {
+			values: [ 'Male', 'Female' ],
+			message: 'Gender is either: Male or Female'
+		},
 		required: [ true, 'A case published must have a gender' ]
 	},
 	underAge: {
@@ -28,8 +37,7 @@ const HomelessSchema = new mongoose.Schema({
 	},
 	age: {
 		type: Number,
-		min: 3,
-		max: 100
+		max: [ 110, 'max age is 110' ]
 	},
 	imageCover: {
 		type: String
